9949afff0ccd851738d54455f8ff540b9602eea5
michal <m.kozel92@gmail.com>
2019-05-24 12:33:26 +0800

    added memoization to squashable words algo

--- recursion/squashable_words.h
+++ recursion/squashable_words.h
@@ -1,11 +1,31 @@
 #include<iostream>
 #include<string>
 #include<unordered_set>
+#include<unordered_map>
 
 using namespace std;
 
-bool is_squashable(string a_word, unordered_set<string> dictionary){
+/**
+ * Function to determine whether a given word is squashable.
+ * A word is squashable if it can be reduced to empty string by removing letters from the original string
+ * in such a way that every newly created word in the process is in a dictionary of valid words
+ *
+ * example: a_dictionary = {'hello', 'hell', 'hel', 'hl', 'l'}
+ * 'hello' can be reduced to empty string through series of valid words and is therefore squashable
+ *
+ * complexity: O(N!)
+ *
+ * @param a_word: the word to examine
+ * @param a_dictionary: dictionary of valid words
+ * @param mem_dict: memoized solutions
+ * @return: whether or not a_word is squashable
+			         */
+bool is_squashable(string a_word, unordered_set<string> dictionary, unordered_map<string, bool> &mem_dict){
   
+  if(mem_dict.find(a_word) != mem_dict.end()){
+    return mem_dict[a_word];
+  }  
+	
   if (dictionary.find(a_word) == dictionary.end()){
     return false;
   }
@@ -14,10 +34,12 @@ bool is_squashable(string a_word, unordered_set<string> dictionary){
   for (int i = 0; i < a_word.length(); i++){
     tmp = a_word;
     tmp.erase(i, 1);
-    if (is_squashable(tmp, dictionary) || tmp.length() == 0){
+    if (is_squashable(tmp, dictionary, mem_dict) || tmp.length() == 0){
+      mem_dict[a_word] = true;
       return true;
     }
   }
+  mem_dict[a_word] = false;
   return false;
 }
 
--- tests/test_is_squashable.cpp
+++ tests/test_is_squashable.cpp
@@ -1,20 +1,23 @@
 #include<gtest/gtest.h>
 #include"../recursion/squashable_words.h"
 #include<unordered_set>
+#include<unordered_map>
 #include<string>
 
 TEST(test_is_squashable, true_tests){
   unordered_set<string> dictionary({"hello", "hell", "hel", "el", "l", "b"});
-  EXPECT_TRUE(is_squashable("hello", dictionary));
-  EXPECT_TRUE(is_squashable("hel", dictionary));
-  EXPECT_TRUE(is_squashable("b", dictionary));
+  unordered_map<string, bool> mem_dict;
+  EXPECT_TRUE(is_squashable("hello", dictionary, mem_dict));
+  EXPECT_TRUE(is_squashable("hel", dictionary, mem_dict));
+  EXPECT_TRUE(is_squashable("b", dictionary, mem_dict));
 }
 
 TEST(test_is_squashable, false_tests){
   unordered_set<string> dictionary({"hello", "hell", "hel", "el", "l", "b"});
-  EXPECT_FALSE(is_squashable("helalo", dictionary));
-  EXPECT_FALSE(is_squashable("heh", dictionary));
-  EXPECT_FALSE(is_squashable("x", dictionary));
+  unordered_map<string, bool> mem_dict;
+  EXPECT_FALSE(is_squashable("helalo", dictionary, mem_dict));
+  EXPECT_FALSE(is_squashable("heh", dictionary, mem_dict));
+  EXPECT_FALSE(is_squashable("x", dictionary, mem_dict));
 }
 
 int main(int argc, char** argv){
