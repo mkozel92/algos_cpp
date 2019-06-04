dfc4f29388d12faaf1416c067c200a46ea1aac76
michal <m.kozel92@gmail.com>
2019-06-04 18:19:14 +0800

    added recursive binary search

--- search/binary_search.h
+++ search/binary_search.h
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+
+
 /**
 * @brief binary search that looks for and elemnt in an sorted array 
 * by halving the array
@@ -10,8 +12,6 @@
 *
 * @return index of the elemnt to search or -1 if this element is not present
 */
-
-
 int my_binary_search(const std::vector<int>& a_vector, int to_search){
   int low = 0;
   int hi = a_vector.size() - 1;
@@ -31,3 +31,33 @@ int my_binary_search(const std::vector<int>& a_vector, int to_search){
   return -1;
 }
 
+
+
+/**
+* @brief recursive implementation of binary search
+* complexity O( log N)
+*
+* @param a_vector a vector to search
+* @param to_searh element to search for
+* @param from_ search from this index
+* @param to_ search to this index
+*
+* @return index of the element or -1
+*/
+int recursive_binary_search(const std::vector<int> &a_vector,\
+                            int to_search, int from_, int to_){
+  if(from_ > to_){
+    return -1;
+  }  
+  int mid = from_ + (to_ - from_) / 2;
+
+  if (a_vector[mid] > to_search){
+    return recursive_binary_search(a_vector, to_search, from_, mid - 1);  
+  }
+  else if (a_vector[mid] < to_search){
+    return recursive_binary_search(a_vector, to_search, mid + 1, to_);  
+  }
+  else{
+    return mid;  
+  }
+}
--- tests/tests_search/test_binary_search.cpp
+++ tests/tests_search/test_binary_search.cpp
@@ -29,3 +29,28 @@ TEST(test_binary_search, test_missing_elements){
   EXPECT_EQ(my_binary_search(v, 150), -1);
 }
 
+TEST(test_binary_search_recursive, test_empty_list){
+  std::vector<int> v;
+  EXPECT_EQ(recursive_binary_search(v, 10, 0, v.size() - 1), -1);  
+}
+
+TEST(test_binary_search_recursive, test_present_elements){
+  std::vector<int> v(100);
+  for(int i = 0; i < v.size(); i++){
+    v[i] = i;
+  }
+  EXPECT_EQ(recursive_binary_search(v, 10, 0, v.size() - 1), 10);
+  EXPECT_EQ(recursive_binary_search(v, 0, 0, v.size() - 1), 0);
+  EXPECT_EQ(recursive_binary_search(v, 99, 0, v.size() - 1), 99);
+}
+
+
+TEST(test_binary_search_recursive, test_missing_elements){
+  std::vector<int> v(100);
+  for(int i = 0; i < v.size(); i++){
+    v[i] = i;
+  }
+  EXPECT_EQ(recursive_binary_search(v, -1, 0, v.size() - 1), -1);
+  EXPECT_EQ(recursive_binary_search(v, 100, 0, v.size() - 1), -1);
+  EXPECT_EQ(recursive_binary_search(v, 150, 0, v.size() - 1), -1);
+}
