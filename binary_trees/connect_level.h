207953d1e79aacd7f082bbf78da907131f45baf8
michal <m.kozel92@gmail.com>
2019-06-15 11:17:59 +0800

    Style fix in connect level algo

--- binary_trees/connect_level.h
+++ binary_trees/connect_level.h
@@ -6,7 +6,7 @@
 #include<queue>
 #include<utility>
 
-namespace binary_tree{
+namespace binary_tree {
 
 /**
 * @brief function to connect node of the tree that are on the same level
@@ -14,24 +14,24 @@ namespace binary_tree{
 *
 * @param a_node root of the tree to connect
 */
-void connect_levels(node_ptr<int, int> a_node){
- std::queue<std::pair<node_ptr<int, int>, int>> q;
- q.push(std::make_pair(a_node, 0));
- 
- std::pair<node_ptr<int, int>, int> a_pair;
- while(!q.empty()){
-  a_pair = q.front();
-  q.pop();
-  if(!q.empty() && a_pair.second == q.front().second){
-    a_pair.first->next = q.front().first;  
+void ConnectLevels(node_ptr<int, int> a_node) {
+  std::queue <std::pair<node_ptr<int, int>, int>> q;
+  q.push(std::make_pair(a_node, 0));
+
+  std::pair<node_ptr<int, int>, int> a_pair;
+  while (!q.empty()) {
+    a_pair = q.front();
+    q.pop();
+    if (!q.empty() && a_pair.second == q.front().second) {
+      a_pair.first->next = q.front().first;
+    }
+    if (a_pair.first->left != nullptr) {
+      q.push(std::make_pair(a_pair.first->left, a_pair.second + 1));
+    }
+    if (a_pair.first->right != nullptr) {
+      q.push(std::make_pair(a_pair.first->right, a_pair.second + 1));
+    }
   }
-  if (a_pair.first->left != nullptr){
-    q.push(std::make_pair(a_pair.first->left, a_pair.second + 1));  
-  }
-  if (a_pair.first->right != nullptr){
-    q.push(std::make_pair(a_pair.first->right, a_pair.second + 1));  
-  }
- }
 }
 
 } // binary tree
